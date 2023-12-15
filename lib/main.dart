42982cc05bc6bf9ad9f2c1a7d5c8ca8b9fb96cb4
Afsal <afsalrahmanafsal989@gmail.com>
2023-12-15 10:55:09 +0530

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Contine Animation"),
+        title: Text("Contine " ),
         backgroundColor: Colors.black,
       ),
       body: GestureDetector(
